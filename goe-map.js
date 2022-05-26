f00554d5d9d598fc8ea387e02da1e68c3e8df4ec
gorrthegod <106343925+gorrthegod@users.noreply.github.com>
2022-05-26 21:58:54 +0300

    Update goe-map.js
    geojson file included

--- goe-map.js
+++ goe-map.js
@@ -12,7 +12,7 @@ function initMap() {
   });
   // NOTE: This uses cross-domain XHR, and may not work on older browsers.
   map.data.loadGeoJson(
-    "https://storage.googleapis.com/mapsdevsite/json/google.json"
+    "https://gorrthegod.github.io/goe/es_counties_simplify.json"
   );
 }
 
